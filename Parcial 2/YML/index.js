d1e0e6f669b3eb5fabc35dadc9b27773dcc9b6d6
SteinRZ <xddan15@gmail.com>
2024-10-02 12:16:02 -0500

    YAML funcional

--- Parcial 2/YML/index.js
+++ Parcial 2/YML/index.js
@@ -3,5 +3,7 @@ const yaml = require('yaml');
 const path = require('path');
 
 const archivo = fs.readFileSync(path.join(__dirname, "./file.yaml"), 'utf-8');
-const estyaml = yaml.parse(archivo);
-console.log(estyaml);
+const estYAML = yaml.parse(archivo);
+console.log(estYAML);
+console.log(typeof estYAML);
+console.table(estYAML);
