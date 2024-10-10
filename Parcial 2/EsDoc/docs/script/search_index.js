8ea2224dd88c758bf5343df663ae1e71048e8bfa
SteinRZ <xddan15@gmail.com>
2024-10-10 18:27:06 -0500

    ESDoc

---
+++ Parcial 2/EsDoc/docs/script/search_index.js
@@ -0,0 +1,20 @@
+window.esdocSearchIndex = [
+  [
+    "esdoc/esdoc/modulo.js~restar",
+    "function/index.html#static-function-restar",
+    "<span>restar</span> <span class=\"search-result-import-path\">esdoc/EsDoc/modulo.js</span>",
+    "function"
+  ],
+  [
+    "esdoc/esdoc/modulo.js~sumar",
+    "function/index.html#static-function-sumar",
+    "<span>sumar</span> <span class=\"search-result-import-path\">esdoc/EsDoc/modulo.js</span>",
+    "function"
+  ],
+  [
+    "esdoc/modulo.js",
+    "file/EsDoc/modulo.js.html",
+    "EsDoc/modulo.js",
+    "file"
+  ]
+]
---
+++ Parcial 2/EsDoc/modulo.js
@@ -0,0 +1,19 @@
+/**
+ * Funcion que toma dos numeros como parametros y devuelve el resultado de la suma de ambos numeros.
+ * @param {number} a Primer numero que se va a sumar
+ * @param {number} b Segundo numero que se va a sumar
+ * @returns {number} El resultado de la suma de los dos numeros
+ */
+export function sumar(a, b) {
+    return a + b;
+}
+
+/**
+ * Funcion que toma dos numeros como parametros y devuelve el resultado de la resta de ambos numeros.
+ * @param {number} a Primer numero que se va a restar
+ * @param {number} b Segundo numero que se va a restar
+ * @returns {number} El resultado de la resta de los dos numeros
+ */
+export function restar(a, b) {
+    return a - b;
+}
