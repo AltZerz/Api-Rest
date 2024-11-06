10a4669950668f7c24debc0f53a0704e6408a19a
SteinRZ <xddan15@gmail.com>
2024-11-06 09:42:32 -0600

    P3 - TestNode

---
+++ Parcial 3/TestNode/index.js
@@ -0,0 +1,8 @@
+import * as areas from "./areas.js";
+import assert from 'node:test';
+import test from 'node:test';
+
+test('Si mando 2, debe regresar 4', () => {
+    let resultado = areas.areaCuadrado(2);
+    assert.strictEqual(resultado,4);
+});
