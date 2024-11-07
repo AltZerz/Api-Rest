b1c192c3995cd8d4451064e2ea75fb8e63de23e5
SteinRZ <xddan15@gmail.com>
2024-11-07 09:38:18 -0600

    P3 - TestNodeChai

--- Parcial 3/TestNodeChai/index.js
+++ Parcial 3/TestNodeChai/index.js
@@ -5,6 +5,7 @@ import test from 'node:test';
 
 test('Si mando un 2, el cuadrado debe ser 4', () => {
     let resultado = areas.areaCuadrado(2);
+    chai.assert.typeOf(resultado,'number');
     chai.assert.equal(resultado,4);
     chai.expect(resultado).to.be.a('number');
 });
