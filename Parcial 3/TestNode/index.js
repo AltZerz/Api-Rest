6669df5ec3e47e9d6cfd4290fbd4eaeda890459e
SteinRZ <xddan15@gmail.com>
2024-11-07 09:25:07 -0600

    P3 - TestNode: Correccion

--- Parcial 3/TestNode/index.js
+++ Parcial 3/TestNode/index.js
@@ -1,5 +1,5 @@
 import * as areas from "./areas.js";
-import assert from 'node:test';
+import assert from 'node:assert';
 import test from 'node:test';
 
 test('Si mando 2, debe regresar 4', () => {
